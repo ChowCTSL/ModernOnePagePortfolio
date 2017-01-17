3f8d7cf419211448307db86b00ec0e7751d9c0af
chowCTSL <chow.ctsl@gmail.com>
2017-01-17 00:28:25 -0700

    Edited nav bar and added resume button

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-
-	$(window).scroll(function() {
-
-		//change navbar color to black on scroll
-		if($(window).scrollTop() != 0) {
-			$('#navbar-main').removeClass('navbar-transparent');
-		}
-
-		//change navbar color to transparent iscrolled to top of the page
-		else if($(window).scrollTop() == 0) {
-			$('#navbar-main').addClass('navbar-transparent');
-		}
-	});
-
 	//scroll when the learn more anchor button is clicked
 	$("a[href^='#']").on('click', function(e) {
 		e.preventDefault();
